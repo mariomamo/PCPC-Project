c560cccb9421b5b9070eb12d5072acf9d0275768
mariomamo <mariooffertucci@live.it>
2021-08-20 23:39:41 +0200

    Calculated bytes for each process

--- projectmain.c
+++ projectmain.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <mpi.h>
 #include <string.h>
-#define NUM_PROCESSES 2
+#define NUM_PROCESSES 13
 #define MAX_FILE_LIST_SIZE 100
 #define MAX_FILE_NAME_LENGTH 255
 #define FILE_NUMBER 5
@@ -40,7 +40,7 @@ FileInfo* getFilesInfos() {
     // char *fileNames[MAX_FILE_NAME_LENGTH] = {
     //     "files/test",
     //     "files/test2",
-    //     "files/test3",
+    //     "files/test3"
     // };
 
     FileInfo *filesInfos = malloc(FILE_NUMBER * sizeof(FileInfo));
@@ -157,7 +157,7 @@ void divideFilesBetweenProcesses(FileInfo *filesInfos, int bytesNumber, int numP
             endToBytes = filesInfos[i].fileSize;
             strcpy(fileToReadInfo.fileName, filesInfos[i].fileName);
             fileToReadInfo.startFromBytes = startFromBytes;
-            fileToReadInfo.endToBytes = endToBytes;
+            fileToReadInfo.endToBytes = endToBytes - 1;
             int inviati = endToBytes - startFromBytes;
             remainingBytesToRead -= inviati;
             // SE IL FILE è TERMINATO
@@ -167,34 +167,43 @@ void divideFilesBetweenProcesses(FileInfo *filesInfos, int bytesNumber, int numP
                 startOffset = 0;
             }
             printf("Processo %d -> {%s} - from %ld to %ld\n", process, fileToReadInfo.fileName, fileToReadInfo.startFromBytes, fileToReadInfo.endToBytes);
+            // printf(">>> INVIATI %ld - %ld = %d\n", endToBytes, startFromBytes, inviati);
+            // printf("\n>>> REMAINING BYTES %ld - %ld = %ld\n", remainingBytesToRead, inviati, r§emainingBytesToRead - inviati);
+            // printf(">>> REMAINING BYTES %ld\n\n", remainingBytesToRead);
         } else if (filesInfos[i].fileSize > remainingBytesToRead) {
             // QUA RESTA IN LOOP
             // printf("FILE SIZE %d, REMAINING %ld\n", filesInfos[i].fileSize, remainingBytesToRead);
             // CI VA SOLO UNA PARTE
             startFromBytes = startOffset;
-            endToBytes = remainingBytesToRead;
+            // printf("\n>>> END TO BYTES %ld + %ld = %ld\n", remainingBytesToRead, startOffset, remainingBytesToRead + startOffset);
+            endToBytes = remainingBytesToRead + startOffset;
             strcpy(fileToReadInfo.fileName, filesInfos[i].fileName);
             fileToReadInfo.startFromBytes = startFromBytes;
-            fileToReadInfo.endToBytes = endToBytes;
-            startOffset = remainingBytesToRead + 1;
+            fileToReadInfo.endToBytes = endToBytes - 1;
             // startOffset = filesInfos[i].fileSize - remainingBytesToRead;
             int inviati = endToBytes - startFromBytes;
             remainingBytesToRead -= inviati;
             // SE IL FILE è TERMINATO
             // SE IL FILE è TERMINATO
             if (inviati == (filesInfos[i].fileSize - startOffset)) {
-                // printf("FILE FINITO 2 \n");
+                // printf("FILE FINITO 2: %d - %d\n", inviati, filesInfos[i].fileSize - startOffset);
                 i++;
+                startOffset = 0;
+            } else {
+                startOffset = endToBytes;
             }
             remainingBytesToRead = bytesPerProcess;
             printf("Processo %d -> {%s} - from %ld to %ld\n", process, fileToReadInfo.fileName, fileToReadInfo.startFromBytes, fileToReadInfo.endToBytes);
+            // printf(">>> [2] INVIATI %ld - %ld = %d\n", endToBytes, startFromBytes, inviati);
+            // printf(">>> [2] REMAINING BYTES %ld - %ld = %ld\n", remainingBytesToRead, inviati, remainingBytesToRead - inviati);
+            // printf(">>> [2] REMAINING BYTES %ld\n\n", remainingBytesToRead);
             process++;
         } else if (filesInfos[i].fileSize == remainingBytesToRead) {
             startFromBytes = startOffset;
             endToBytes = remainingBytesToRead;
             strcpy(fileToReadInfo.fileName, filesInfos[i].fileName);
             fileToReadInfo.startFromBytes = startOffset;
-            fileToReadInfo.endToBytes = endToBytes;
+            fileToReadInfo.endToBytes = endToBytes - 1;
             int inviati = endToBytes - startFromBytes;
             remainingBytesToRead -= inviati;
             // SE IL FILE è TERMINATO
@@ -205,6 +214,7 @@ void divideFilesBetweenProcesses(FileInfo *filesInfos, int bytesNumber, int numP
             remainingBytesToRead = bytesPerProcess;
             printf("Processo %d -> {%s} - from %ld to %ld\n", process, fileToReadInfo.fileName, fileToReadInfo.startFromBytes, fileToReadInfo.endToBytes);
         }
+        // printf("%d - %d\n", i < numFiles, process < NUM_PROCESSES);
     }
     fileToReadInfo.startFromBytes = 0;
     fileToReadInfo.endToBytes = 100;
